b885385bdc33ad877252d3b79790c41a7afea86a
Eiko <gaia.e.h@gmail.com>
2019-10-15 23:36:59 +0800

    大概框架

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,24 @@
 "use strict";
 console.log('script.js loaded');
 let songList;
-$.ajax({
-    type: "GET",
-    url: "./data/lite.json",
-    dataType: "json",
-    success: function (response) {
-        songList = response;
-        console.log(songList);
-    }
+$(document).ready(function () {
+    // 载入歌单
+    $.ajax({
+        type: "GET",
+        url: "./data/lite.json",
+        dataType: "json",
+        success: function (response) {
+            songList = response;
+            console.log(songList);
+        }
+    });
+
+    // 设置相关
+    $(document).on('click', '#option-button', function () {
+        $('.option-main').addClass('show');
+    });
+    $(document).on('submit', '#option-form', function () {
+        $('.option-main').removeClass('show');
+        return false;
+    });
 });
