73a8e84eb31b00438e7170d047df4df645afb5a7
Eiko <gaia.e.h@gmail.com>
2019-10-16 00:27:57 +0800

    简单写了设置

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 console.log('script.js loaded');
-let songList;
+let
+    songList,
+    option = {};
 $(document).ready(function () {
     // 载入歌单
     $.ajax({
@@ -18,6 +20,24 @@ $(document).ready(function () {
         $('.option-main').addClass('show');
     });
     $(document).on('submit', '#option-form', function () {
+        let formDataArray = $(this).serializeArray();
+        console.log(formDataArray);
+        let temp = formDataArray.filter(item => item.name == 'level');
+        console.log(temp);
+        option.levels = [];
+        temp.map(item => option.levels.push(item.value));
+        console.log(option.levels);
+        if (option.levels.length !== 0) {
+            $('#option-show-level').text(option.levels.join('、'));
+        } else {
+            $('#option-show-level').text('未设置');
+        }
+
+        option.levelMin = formDataArray.find(item => item.name == "level-num-min").value;
+        $('#option-show-level-num-min').text(option.levelMin);
+        option.levelMax = formDataArray.find(item => item.name == "level-num-max").value;
+        $('#option-show-level-num-max').text(option.levelMax);
+
         $('.option-main').removeClass('show');
         return false;
     });
