7b2616ebfe98f6b3e29a6743a39d4eca2d9aaf90
Eiko <gaia.e.h@gmail.com>
2019-10-23 03:26:28 +0800

    能用了，牛逼

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,14 @@
 console.log('script.js loaded');
 let
     songList = {},
-    option = {};
+    option = {},
+    difficultyText = {
+        'easy': '梅',
+        'normal': '竹',
+        'hard': '松',
+        'oni': '鬼'
+    },
+    temp;
 $(document).ready(function () {
     // 载入歌单
     let loadFileCount = 0;
@@ -43,11 +50,11 @@ $(document).ready(function () {
         console.log(formDataArray);
         let temp = formDataArray.filter(item => item.name == 'level');
         console.log(temp);
-        option.levels = [];
-        temp.map(item => option.levels.push(item.value));
-        console.log(option.levels);
-        if (option.levels.length !== 0) {
-            $('#option-show-level').text(option.levels.join('、'));
+        option.difficulty = [];
+        temp.map(item => option.difficulty.push(item.value));
+        console.log(option.difficulty);
+        if (option.difficulty.length !== 0) {
+            $('#option-show-level').text(difficultyText[option.difficulty]);
         } else {
             $('#option-show-level').text('未设置');
         }
@@ -58,6 +65,45 @@ $(document).ready(function () {
         $('#option-show-level-num-max').text(option.levelMax);
 
         $('.option-main').removeClass('show');
+        $('#roll-button').prop('disabled', false);
         return false;
+    });
+
+    // 抽奖
+    function selectFrom(lowerValue, upperValue) {
+        let choices = upperValue - lowerValue + 1;
+        return Math.floor(Math.random() * choices + lowerValue);
+    }
+    $(document).on('click', '#roll-button', function () {
+        console.log('Roll!');
+        $('#roll-button').prop('disabled', true);
+        let selectedSong, theA = 0;
+        function rollSong() {
+            let random = selectFrom(0, songList['taiko14old'].songList.length - 1);
+            selectedSong = songList['taiko14old'].songList[random];
+            console.log(selectedSong);
+            temp = selectedSong;
+            if (selectedSong.level[option.difficulty] >= option.levelMin && selectedSong.level[option.difficulty] <= option.levelMax) {
+                console.log('---');
+                console.log(selectedSong);
+                $('#result-name').text(selectedSong.name);
+                $('#result-name-cn').text(selectedSong.name_cn);
+                $('#result-category').text(selectedSong.category);
+                $('#result-level-name').text(difficultyText[option.difficulty]);
+                $('#result-level-num').text(selectedSong.level[option.difficulty]);
+                theA++;
+                console.log(theA);
+                if (theA > 20) {
+                    $('#roll-button').prop('disabled', false);
+                    theA = 0;
+                }
+            } else {
+                console.log('Roll fail');
+                rollSong();
+            }
+        }
+        for (let a = 0; a <= 20; a++) {
+            setTimeout(rollSong, 100 * a);
+        }
     });
 });
